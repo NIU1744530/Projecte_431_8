b9554e6043a71accca0b3957eb25322f7d8b6c68
Alex Ponsa <1744530@uab.cat>
2025-05-06 11:11:07 +0200

    enums.h

---
+++ enums.h
@@ -0,0 +1,20 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+typedef enum
+{
+    TIPUS_NORMAL,
+    TIPUS_DAMA,
+    TIPUS_EMPTY
+}
+TipusFitxa;
+
+typedef enum
+{
+    COLOR_NEGRE,
+    COLOR_BLANC,
+    COLOR_EMPTY 
+}
+ColorFitxa;
+ 
+#endif
