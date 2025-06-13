319dd6e0fdd0136d8cde4e42830d052542ec8d24
Alex Ponsa <1744530@uab.cat>
2025-06-13 18:57:49 +0200

    Actualiza tauler.hpp

--- tauler.hpp
+++ tauler.hpp
@@ -1,4 +1,4 @@
-#ifndef TAULER_HPP
+﻿#ifndef TAULER_HPP
 #define TAULER_HPP
 
 #include "fitxa.h"
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+
 
 using namespace std;
 
@@ -18,20 +20,29 @@ class Tauler
 {
 public:
     Tauler();
+    ~Tauler();
+
     void inicialitza(const string& nomFitxer);
     void actualitzaMovimentsValids();
     bool mouFitxa(const Posicio& origen, const Posicio& desti);
-    void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]) const;
+    vector<Posicio> getPosicionsPossibles(const Posicio& origen) const;
     string toString() const;
 
+    void visualitza() const;
+    Fitxa& getFitxa(const Posicio& pos) { return *m_tauler[pos.getFila()][pos.getColumna()]; } 
+    int comptaFitxes(ColorFitxa color) const;
+    ColorFitxa comprovaGuanyador() const;
+
+
+
 private:
-    Fitxa m_tauler[N_FILES][N_COLUMNES];
+    Fitxa* m_tauler[N_FILES][N_COLUMNES];
 
     bool validaPosicio(const Posicio& posicio) const;
     bool esPosicioBuida(const Posicio& posicio) const;
     void actualitzaMovimentsFitxa(const Posicio& posicio);
     void buscaMovimentsCaptura(const Posicio& posicio, Fitxa& fitxa);
-    void buscaCapturesRecursiu(const Posicio& posActual, ColorFitxa color, bool esDama, Moviment movimentActual, Moviment movimentsCaptura[], int& numMoviments);
+    void buscaCapturesRecursiu(const Posicio& posActual, ColorFitxa color, bool esDama, Moviment movimentActual, vector<Moviment>& movimentsCapturas);
     void eliminaFitxa(const Posicio& posicio);
     void mouFitxaInterna(const Posicio& origen, const Posicio& desti);
 };
